b6da3acdaaf38f23790f2b4ad80d11502f963672
Luiz Andrade <ltiburci@villanova.edu>
2022-07-12 21:31:18 +0000

    initial commit basic test

---
+++ test_basic.py
@@ -0,0 +1,19 @@
+sys.path.append('../week3') # imports python file from parent directory
+from demo import app #imports flask app object
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
